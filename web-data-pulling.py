19a0dad5448adabb6cde23b88460094d90eaeef9
HarryPommier <harry.pommier@zenika.com>
2020-02-20 15:12:09 +0100

    :bug: fix species with no images, avoid camera.png

--- web-data-pulling.py
+++ web-data-pulling.py
@@ -9,7 +9,7 @@ species = {"src": [], "label_en": [], "label_latin": []}
 url = "https://www.wood-database.com/wood-finder/?fwp_paged="
 
 #woodfinder 37 pages
-for k in range(37):
+for k in range(2):
     print("Pulling page {}...".format(k))
     response = urllib.request.urlopen(url+str(k))
     html = response.read()
@@ -19,13 +19,15 @@ for k in range(37):
     images = soup.find_all("img", {"class": "attachment-banner size-banner wp-post-image"})
     for image in images:
         src = image["src"]
-        label_en = image.find_next("strong").text
-        label_latin = image.find_next("em").text
-        label_en = "".join([ c if (c.isalpha() or " ") else "" for c in label_en]).lower().strip(".,()")
-        label_latin = "".join([ c if (c.isalpha() or " ") else "" for c in label_latin]).lower().strip(".,()")
-        species["src"].append(src)
-        species["label_en"].append(label_en)
-        species["label_latin"].append(label_latin)
+        if "camera.png" not in src:
+            label_en = image.find_next("strong").text
+            label_latin = image.find_next("em").text
+            label_en = "".join([ c if (c.isalpha() or " ") else "" for c in label_en]).lower().strip(".,()")
+            label_latin = "".join([ c if (c.isalpha() or " ") else "" for c in label_latin]).lower().strip(".,()")
+            species["src"].append(src)
+            species["label_en"].append(label_en)
+            species["label_latin"].append(label_latin)
+    print(species)
 
 #download images
 datadir_name = "databases"
